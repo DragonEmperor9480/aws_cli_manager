b3ccd02615494bffbbb79c53364a4c640da21ea1
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-05-31 16:16:10 +0530

    utils: update `processing` animation

--- utils/processing_animation.go
+++ utils/processing_animation.go
@@ -5,11 +5,22 @@ import (
 	"time"
 )
 
-func ShowProcessingAnimation(duration time.Duration) {
-	frames := []string{"|", "/", "-", "\\"}
-	for i := 0; i < int(duration.Seconds()*4); i++ {
-		fmt.Printf("\rProcessing... %s", frames[i%len(frames)])
-		time.Sleep(250 * time.Millisecond)
-	}
-	fmt.Print("\rDone!           \n")
+var animationRunning = false
+
+func ShowProcessingAnimation(message string) {
+	animationRunning = true
+	go func() {
+		frames := []string{"|", "/", "-", "\\"}
+		i := 0
+		for animationRunning {
+			fmt.Printf("\r%s %s", Blue+message+Reset, frames[i%len(frames)])
+			time.Sleep(100 * time.Millisecond)
+			i++
+		}
+	}()
+}
+
+func StopAnimation() {
+	animationRunning = false
+	fmt.Print("\r                                \r") // Clear the line
 }
