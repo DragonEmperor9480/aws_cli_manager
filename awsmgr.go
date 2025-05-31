2e8a05c0ab8aa600faf313370d548bb2741ec170
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-05-31 15:15:04 +0530

    awsmgr: initial `menu` design

--- awsmgr.go
+++ awsmgr.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"os/exec"
+	"os"
+	"strings"
+
+	"github.com/DragonEmperor9480/aws_cli_manager/views"
 )
 
 func main() {
-	cmd := exec.Command("echo", "Hello World")
-	output, err := cmd.Output()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		views.ShowMenu()
+
+		fmt.Print("Select option [1-4]: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			fmt.Println()
+			fmt.Println("IAM MANAGEMENT")
+			fmt.Println("────────────────────────────────────")
+			//controllers.iam_manager()
+		case "2":
+			fmt.Println()
+			fmt.Println("EC2 MANAGEMENT")
+			fmt.Println("────────────────────────────────────")
+			//controllers.ec2_manager()
+		case "3":
+			fmt.Println()
+			fmt.Println("S3 MANAGEMENT")
+			fmt.Println("────────────────────────────────────")
+			//controllers.s3_manager()
+		case "4":
+			fmt.Println("\nExiting AWS CLI Manager...")
+			return
+		default:
+			fmt.Println("\nInvalid selection. Please try again.")
+		}
 	}
-	fmt.Println(string(output))
 }
---
+++ views/rootview.go
@@ -0,0 +1,19 @@
+package views
+
+import (
+	"github.com/DragonEmperor9480/aws_cli_manager/utils"
+	"fmt"
+)
+
+func ShowMenu() {
+	fmt.Println(utils.Bold + utils.Blue + "┌─────────────────────────────────┐" + utils.Reset)
+	fmt.Println(utils.Bold + utils.Blue + "│        AWS CLI MANAGER v1.0.0   │ " + utils.Cyan + "STABLE" + utils.Reset)
+	fmt.Println(utils.Bold + utils.Blue + "└─────────────────────────────────┘" + utils.Reset)
+	fmt.Println(utils.Cyan + "Available services:" + utils.Reset)
+	fmt.Println("────────────────────────────────────")
+	fmt.Println(utils.Bold + utils.Blue + "[1]" + utils.Reset + " " + utils.Bold + "IAM" + utils.Reset + " - Identity and Access Management")
+	fmt.Println(utils.Bold + utils.Blue + "[2]" + utils.Reset + " " + utils.Bold + "EC2" + utils.Reset + " - Elastic Compute Cloud")
+	fmt.Println(utils.Bold + utils.Blue + "[3]" + utils.Reset + " " + utils.Bold + "S3" + utils.Reset + "  - Simple Storage Service")
+	fmt.Println(utils.Bold + utils.Red + "[4]" + utils.Reset + " " + utils.Bold + "Exit" + utils.Reset)
+	fmt.Println("────────────────────────────────────")
+}
