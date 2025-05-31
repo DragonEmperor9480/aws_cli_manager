bddf5a00652c8b3c69b9f2c21d10fdfb0908bba3
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-05-31 14:47:49 +0530

    Initialize AWS CLI Manager project in Golang

---
+++ utils/colors.go
@@ -0,0 +1,13 @@
+// utils/colors.go
+package utils
+
+const (
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Cyan    = "\033[36m"
+	Bold    = "\033[1m"
+	Reset   = "\033[0m"
+	Magenta = "\033[35m"
+)
