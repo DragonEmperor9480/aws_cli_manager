2577bf576edc82f177d5279e682fbe3ee7dd4b61
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-07-21 21:51:43 +0530

    model: iam: user: add animation to `UserExistsOrNotModel`

--- models/iam/user/userExistsOrNotModel.go
+++ models/iam/user/userExistsOrNotModel.go
@@ -3,14 +3,19 @@ package user
 import (
 	"os/exec"
 	"strings"
+
+	utils "github.com/DragonEmperor9480/aws_cli_manager/utils"
 )
 
 func UserExistsOrNotModel(username string) bool {
+	utils.ShowProcessingAnimation("Checking if user exists: " + username)
 	checkCmd := exec.Command("aws", "iam", "get-user", "--user-name", username)
-    output, _ := checkCmd.CombinedOutput()
-    if strings.Contains(string(output), "NoSuchEntity") {
-        return false
-    }
-    return true
-}			
+	output, _ := checkCmd.CombinedOutput()
+	if strings.Contains(string(output), "NoSuchEntity") {
+		utils.StopAnimation()
+		return false
+	}
+	utils.StopAnimation()
+	return true
 
+}
