7ee3247d3d2032d2f112d7cca7db1204b0e35be7
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-11-11 21:33:35 +0530

    awsmgr: cloudwatch: add  cloudwatch manager

---
+++ controllers/cloudwatch_mgr.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/DragonEmperor9480/aws_cli_manager/utils"
+	cloudwatchview "github.com/DragonEmperor9480/aws_cli_manager/views/cloudwatch"
+)
+
+func CloudWatch_mgr() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		cloudwatchview.ShowCloudWatchMenu()
+
+		fmt.Print("Enter your choice: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			 // still need to add livetail functionality uff
+			utils.Bk()
+		case "2":
+			// Back to main menu
+			fmt.Println("Returning to Main Menu...")
+			return
+		default:
+			fmt.Println(utils.Red + "Invalid Input. Please try again." + utils.Reset)
+			utils.Bk()
+		}
+	}
+}
