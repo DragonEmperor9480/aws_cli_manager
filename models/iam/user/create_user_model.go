c8effbab72fb1a10a55864d7b006ddb8759fbb93
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-11-13 21:57:18 +0530

    awsmgr: iam: switch from aws cli to aws sdk for create user

--- models/iam/user/create_user_model.go
+++ models/iam/user/create_user_model.go
@@ -1,34 +1,38 @@
 package user
 
 import (
+	"context"
 	"fmt"
-	"os/exec"
 	"strings"
 
 	"github.com/DragonEmperor9480/aws_cli_manager/utils"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
 )
 
 func CreateIAMUser(username string) {
 	// Start animation in background
 	utils.ShowProcessingAnimation("Creating IAM User")
 
-	// Execute AWS command
-	cmd := exec.Command("aws", "iam", "create-user", "--user-name", username)
-	outputBytes, _ := cmd.CombinedOutput()
-	output := string(outputBytes)
+	// Execute AWS SDK call
+	ctx := context.TODO()
+	_, err := utils.IAMClient.CreateUser(ctx, &iam.CreateUserInput{
+		UserName: aws.String(username),
+	})
 
 	// Stop animation and print a newline
 	utils.StopAnimation()
 	fmt.Println()
 
-	// Handle output
-	if strings.Contains(output, "EntityAlreadyExists") {
-		fmt.Println(utils.Bold + utils.Red + "Error: User '" + username + "' already exists!" + utils.Reset)
+	if err != nil {
+		if strings.Contains(err.Error(), "EntityAlreadyExists") {
+			fmt.Println(utils.Bold + utils.Red + "Error: User '" + username + "' already exists!" + utils.Reset)
+		} else {
+			fmt.Println(utils.Yellow + "Unexpected error occurred:" + utils.Reset)
+			fmt.Println(err.Error())
+		}
 		return
-	} else if strings.Contains(output, "UserName") {
-		fmt.Println(utils.Bold + utils.Green + "User '" + username + "' created successfully!" + utils.Reset)
-	} else {
-		fmt.Println(utils.Yellow + "Unexpected error occurred:" + utils.Reset)
-		fmt.Println(output)
 	}
+
+	fmt.Println(utils.Bold + utils.Green + "User '" + username + "' created successfully!" + utils.Reset)
 }
